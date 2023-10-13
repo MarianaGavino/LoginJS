59dea13565f3c7893c110eb3569f4baeba24f25d
MarianaGavino <marianagavinohdz@gmail.com>
2023-10-13 17:30:33 -0600

    tabla

--- home.js
+++ home.js
@@ -1,1 +1,8 @@
-const table = document.querySelector(".table");
+const div = document.querySelector(".content");
+const tbody =document.querySelector(".tbody");
+
+const datos = [1, 2, 3, 4, 5];
+
+let tableNumbers = datos.map((num) => `<tr><td style="color: ${num%2 === 0 ? "yellow" : "green"}">${num}</td></tr>`)
+
+tbody.innerHTML = tableNumbers;
