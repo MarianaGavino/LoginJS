6cdccea1d41b3c117f19494214055510c13c6124
MarianaGavino <marianagavinohdz@gmail.com>
2023-10-16 09:21:16 -0600

    Eliminacion lineas sin uso

--- home.js
+++ home.js
@@ -1,8 +1,7 @@
-const div = document.querySelector(".content");
 const tbody =document.querySelector(".tbody");
 
 const datos = [1, 2, 3, 4, 5];
 
-let tableNumbers = datos.map((num) => `<tr><td style="color: ${num%2 === 0 ? "yellow" : "green"}">${num}</td></tr>`)
+let tableNumbers = datos.map((num) => `<tr><td style="color: ${num%2 === 0 ? "yellow" : "green"}">Fila: ${num}</td></tr>`)
 
 tbody.innerHTML = tableNumbers;
